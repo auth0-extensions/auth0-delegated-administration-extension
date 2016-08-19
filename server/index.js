90615ccfab3783e4af5adba1979851d7e68ee988
Sandrino Di Mattia <contact@sandrino.be>
2016-08-18 17:08:55 -0700

    v1.2.0 - Fix issues with extension tooling

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import path from 'path';
 import morgan from 'morgan';
 import Express from 'express';
 import bodyParser from 'body-parser';
-import { toConfigProvider } from 'auth0-extension-tools';
+import { configProvider } from 'auth0-extension-tools';
 
 import api from './routes/api';
 import hooks from './routes/hooks';
@@ -13,13 +13,13 @@ import config from './lib/config';
 import logger from './lib/logger';
 import * as middlewares from './lib/middlewares';
 
-module.exports = (configProvider) => {
-  config.setProvider(configProvider);
+module.exports = (cfg) => {
+  config.setProvider(cfg);
 
   const app = new Express();
   app.use((req, res, next) => {
     if (req.webtaskContext) {
-      config.setProvider(toConfigProvider(req.webtaskContext));
+      config.setProvider(configProvider.fromWebtaskContext(req.webtaskContext));
     }
 
     next();
--- webtask.js
+++ webtask.js
@@ -3,7 +3,7 @@ const tools = require('auth0-extension-express-tools');
 const expressApp = require('./server');
 const logger = require('./server/lib/logger');
 
-module.exports = tools.createExpressServer(function(req, config, storage) {
+module.exports = tools.createServer(function(req, config, storage) {
   logger.info('Starting Delegated Administration extension - Version:', config('CLIENT_VERSION'));
   return expressApp(config, storage);
 });
