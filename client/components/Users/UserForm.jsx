010796a566641e1b4420dcc631bee92f1fa42a62
roman.bazarov <trexbazarov@gmail.com>
2016-09-02 18:28:08 +0300

    user creation

--- client/components/Users/UserForm.jsx
+++ client/components/Users/UserForm.jsx
@@ -33,7 +33,7 @@ export default class UserForm extends Component {
         let connection = _.find(this.props.connections, function (connection) {
             return connection.name == e.target.value;
         });
-        if(connection&&connection.options&&connection.options.requires_username) {
+        if (connection && connection.options && connection.options.requires_username) {
             this.setState({
                 usernameRequired: true
             });
@@ -48,27 +48,28 @@ export default class UserForm extends Component {
         if (this.props.loading || this.props.error) {
             return <div></div>;
         }
-        const connections = _.filter(this.props.connections, function(connection) {
+        const connections = _.filter(this.props.connections, function (connection) {
             return connection.strategy == 'auth0';
         });
         const usernameRequired = this.state.usernameRequired;
         return <div className="row">
-            <form className="createUserScreenForm form-horizontal col-xs-12" style={{marginTop: '40px'}} onSubmit={function (e) {
-                e.preventDefault();
-                let arr = $('.createUserScreenForm').serializeArray(), obj = {};
-                $.each(arr, function (indx, el) {
-                    if (el.name != 'repeat_password') {
-                        obj[el.name] = el.value;
-                    }
-                });
-                obj["email_verified"] = true;
-                this.props.createUser(obj, function () {
-                    this.props.userWasSaved();
-                    setTimeout(function () {
-                        this.props.fetchUsers('', true);
-                    }.bind(this), 500);
-                }.bind(this));
-            }.bind(this)}>
+            <form className="createUserScreenForm form-horizontal col-xs-12" style={{marginTop: '40px'}}
+                  onSubmit={function (e) {
+                      e.preventDefault();
+                      let arr = $('.createUserScreenForm').serializeArray(), obj = {};
+                      $.each(arr, function (indx, el) {
+                          if (el.name != 'repeat_password') {
+                              obj[el.name] = el.value;
+                          }
+                      });
+                      obj["email_verified"] = true;
+                      this.props.createUser(obj, function () {
+                          this.props.userWasSaved();
+                          setTimeout(function () {
+                              this.props.fetchUsers('', true);
+                          }.bind(this), 500);
+                      }.bind(this));
+                  }.bind(this)}>
                 <div className="form-group">
                     <label className="col-xs-2 control-label">Email</label>
                     <div className="col-xs-9">
@@ -100,7 +101,8 @@ export default class UserForm extends Component {
                 <div className="form-group row">
                     <label className="col-xs-2 control-label">Connection</label>
                     <div className="col-xs-9">
-                        <select className="form-control" name="connection" onChange={this.onConnectionChange.bind(this)}>
+                        <select className="form-control" name="connection"
+                                onChange={this.onConnectionChange.bind(this)}>
                             <option value="">Select...</option>
                             {connections.map((connection, index) => {
                                 return <option key={index}
