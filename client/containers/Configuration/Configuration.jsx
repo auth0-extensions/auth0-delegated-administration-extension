7948886e0d156408081a524bc96e99730e883cb1
roman.bazarov <trexbazarov@gmail.com>
2016-09-20 13:14:24 +0300

    lint errors.Remove set state from shouldComponentUpdate

--- client/containers/Configuration/Configuration.jsx
+++ client/containers/Configuration/Configuration.jsx
@@ -19,32 +19,12 @@ class Configuration extends Component {
 
     this.state = {
       activeTab: 1,
-      scripts: props.scripts || { },
-      errors: { }
+      scripts: props.scripts || {},
+      errors: {}
     };
   }
 
   shouldComponentUpdate(nextProps) {
-    if (nextProps.updatedScript) {
-      let errors = this.state.errors;
-      if (nextProps.updateError) {
-        if (!errors[nextProps.updatedScript] || errors[nextProps.updatedScript] !== nextProps.updateError) {
-          errors[nextProps.updatedScript] = nextProps.updateError;
-          this.setState({
-            errors: errors
-          });
-          return true;
-        }
-      } else {
-        if (errors[nextProps.updatedScript]) {
-          errors[nextProps.updatedScript] = null;
-          this.setState({
-            errors: errors
-          });
-          return true;
-        }
-      }
-    }
     return this.props.scripts !== nextProps.scripts ||
       this.props.updateError !== nextProps.updateError ||
       this.props.updatedScript !== nextProps.updatedScript ||
@@ -60,6 +40,28 @@ class Configuration extends Component {
   };
 
   componentWillReceiveProps(nextProps) {
+    if (nextProps.updatedScript) {
+      let errors = this.state.errors;
+      if (nextProps.updateError) {
+        if (!errors[nextProps.updatedScript] || errors[nextProps.updatedScript] !== nextProps.updateError) {
+          errors[nextProps.updatedScript] = nextProps.updateError;
+          this.setState({
+            errors: errors,
+            scripts: nextProps.scripts.toJS()
+          });
+          return;
+        }
+      } else {
+        if (errors[nextProps.updatedScript]) {
+          errors[nextProps.updatedScript] = null;
+          this.setState({
+            errors: errors,
+            scripts: nextProps.scripts.toJS()
+          });
+          return;
+        }
+      }
+    }
     this.setState({
       scripts: nextProps.scripts.toJS()
     });
@@ -87,7 +89,7 @@ class Configuration extends Component {
   };
 
   render() {
-    const { scripts, loading, error, updateLoading, updatedScript  } = this.props;
+    const { loading, error, updateLoading, updatedScript } = this.props;
     return (
       <LoadingPanel show={loading} animationStyle={{ paddingTop: '5px', paddingBottom: '5px' }}>
         <Error message={error} />
@@ -95,79 +97,93 @@ class Configuration extends Component {
           <div className="row content-header">
             <div className="col-xs-12 userTableContent">
               <h2>Configuration</h2>
-              <p>This configuration page allows you to fine tune the behavior of the Delegated Administration dashboard.</p>
+              <p>This configuration page allows you to fine tune the behavior of the Delegated Administration
+                dashboard.</p>
             </div>
           </div>
           <div className="row user-tabs">
             <div className="col-xs-12">
               <Tabs defaultActiveKey={this.state.activeTab} animation={false}>
                 <Tab eventKey={1} title="Filter Hook">
-                  <LoadingPanel show={updateLoading&&updatedScript === 'filter'} animationStyle={{ paddingTop: '5px', paddingBottom: '5px' }}>
-                  <p>The <strong>filter hook</strong> allows you to specify which records are shown to the current users when loading the list of users or searching. For example: <i>Only show users from my department</i>. This has to be defined using the lucene syntax.</p>
-                  <Error message={this.state.errors.filter} />
-                  <Editor
-                    value={String(this.state.scripts.filter)}
-                    onChange={this.updateScript('filter')}
-                  />
-                  <div className="saveConfigurationButton">
-                    <button onClick={this.saveScript('filter')} className="btn btn-success">Save Filter Hook
-                    </button>
-                  </div>
+                  <LoadingPanel show={updateLoading && updatedScript === 'filter'}
+                                animationStyle={{ paddingTop: '5px', paddingBottom: '5px' }}>
+                    <p>The <strong>filter hook</strong> allows you to specify which records are shown to the current
+                      users when loading the list of users or searching. For example: <i>Only show users from my
+                        department</i>. This has to be defined using the lucene syntax.</p>
+                    <Error message={this.state.errors.filter} />
+                    <Editor
+                      value={String(this.state.scripts.filter)}
+                      onChange={this.updateScript('filter')}
+                    />
+                    <div className="saveConfigurationButton">
+                      <button onClick={this.saveScript('filter')} className="btn btn-success">Save Filter Hook
+                      </button>
+                    </div>
                   </LoadingPanel>
                 </Tab>
                 <Tab eventKey={2} title="Access Hook">
-                  <LoadingPanel show={updateLoading&&updatedScript === 'access'} animationStyle={{ paddingTop: '5px', paddingBottom: '5px' }}>
-                  <p>The <strong>access hook</strong> will allow you to specify if the current user is allowed to access a specific user (eg: view the details, delete the user, ...).</p>
-                  <Error message={this.state.errors.access} />
-                  <Editor
-                    value={String(this.state.scripts.access)}
-                    onChange={this.updateScript('access')}
-                  />
-                  <div className="saveConfigurationButton">
-                    <button onClick={this.saveScript('access')} className="btn btn-success">Save Access Hook</button>
-                  </div>
+                  <LoadingPanel show={updateLoading && updatedScript === 'access'}
+                                animationStyle={{ paddingTop: '5px', paddingBottom: '5px' }}>
+                    <p>The <strong>access hook</strong> will allow you to specify if the current user is allowed to
+                      access a specific user (eg: view the details, delete the user, ...).</p>
+                    <Error message={this.state.errors.access} />
+                    <Editor
+                      value={String(this.state.scripts.access)}
+                      onChange={this.updateScript('access')}
+                    />
+                    <div className="saveConfigurationButton">
+                      <button onClick={this.saveScript('access')} className="btn btn-success">Save Access Hook</button>
+                    </div>
                   </LoadingPanel>
                 </Tab>
                 <Tab eventKey={4} title="Create Hook">
-                  <LoadingPanel show={updateLoading&&updatedScript === 'create'} animationStyle={{ paddingTop: '5px', paddingBottom: '5px' }}>
-                  <p>The <strong>create hook</strong> will run every time a new user is created. This hook will allow you to shape the user object before it's sent to Auth0. The context object contains the request (with the current user) and the payload sent by the end user.</p>
-                  <Error message={this.state.errors.create} />
-                  <Editor
-                    value={String(this.state.scripts.create)}
-                    onChange={this.updateScript('create')}
-                  />
-                  <div className="saveConfigurationButton">
-                    <button onClick={this.saveScript('create')} className="btn btn-success">Save Create Query
-                    </button>
-                  </div>
+                  <LoadingPanel show={updateLoading && updatedScript === 'create'}
+                                animationStyle={{ paddingTop: '5px', paddingBottom: '5px' }}>
+                    <p>The <strong>create hook</strong> will run every time a new user is created. This hook will allow
+                      you to shape the user object before it's sent to Auth0. The context object contains the request
+                      (with the current user) and the payload sent by the end user.</p>
+                    <Error message={this.state.errors.create} />
+                    <Editor
+                      value={String(this.state.scripts.create)}
+                      onChange={this.updateScript('create')}
+                    />
+                    <div className="saveConfigurationButton">
+                      <button onClick={this.saveScript('create')} className="btn btn-success">Save Create Query
+                      </button>
+                    </div>
                   </LoadingPanel>
                 </Tab>
                 <Tab eventKey={3} title="Memberships Query">
-                  <LoadingPanel show={updateLoading&&updatedScript === 'memberships'} animationStyle={{ paddingTop: '5px', paddingBottom: '5px' }}>
-                  <p>With the <strong>membership query</strong> you can specify in which groups the current user can create new users. Only in their own department? Or other departments also?</p>
-                  <Error message={this.state.errors.memberships} />
-                  <Editor
-                    value={String(this.state.scripts.memberships)}
-                    onChange={this.updateScript('memberships')}
-                  />
-                  <div className="saveConfigurationButton">
-                    <button onClick={this.saveScript('memberships')} className="btn btn-success">Save Memberships Query
-                    </button>
-                  </div>
+                  <LoadingPanel show={updateLoading && updatedScript === 'memberships'}
+                                animationStyle={{ paddingTop: '5px', paddingBottom: '5px' }}>
+                    <p>With the <strong>membership query</strong> you can specify in which groups the current user can
+                      create new users. Only in their own department? Or other departments also?</p>
+                    <Error message={this.state.errors.memberships} />
+                    <Editor
+                      value={String(this.state.scripts.memberships)}
+                      onChange={this.updateScript('memberships')}
+                    />
+                    <div className="saveConfigurationButton">
+                      <button onClick={this.saveScript('memberships')} className="btn btn-success">Save Memberships
+                        Query
+                      </button>
+                    </div>
                   </LoadingPanel>
                 </Tab>
                 <Tab eventKey={5} title="Settings Query">
-                  <LoadingPanel show={updateLoading&&updatedScript === 'settings'} animationStyle={{ paddingTop: '5px', paddingBottom: '5px' }}>
-                  <p>With the <strong>settings query</strong> you can control the title and the look-and-feel of the dashboard after the user has logged in?</p>
-                  <Error message={this.state.errors.settings} />
-                  <Editor
-                    value={String(this.state.scripts.settings)}
-                    onChange={this.updateScript('settings')}
-                  />
-                  <div className="saveConfigurationButton">
-                    <button onClick={this.saveScript('settings')} className="btn btn-success">Save Settings Query
-                    </button>
-                  </div>
+                  <LoadingPanel show={updateLoading && updatedScript === 'settings'}
+                                animationStyle={{ paddingTop: '5px', paddingBottom: '5px' }}>
+                    <p>With the <strong>settings query</strong> you can control the title and the look-and-feel of the
+                      dashboard after the user has logged in?</p>
+                    <Error message={this.state.errors.settings} />
+                    <Editor
+                      value={String(this.state.scripts.settings)}
+                      onChange={this.updateScript('settings')}
+                    />
+                    <div className="saveConfigurationButton">
+                      <button onClick={this.saveScript('settings')} className="btn btn-success">Save Settings Query
+                      </button>
+                    </div>
                   </LoadingPanel>
                 </Tab>
               </Tabs>
