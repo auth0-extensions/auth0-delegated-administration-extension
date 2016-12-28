006f488af381fc1dee0e0c11f1eb2f5660665e5c
Oleksandr Zarubin <oleksandr.zarubin@ua.akvelon.com>
2016-12-28 14:22:58 +0200

    back compatibility

--- server/constants.js
+++ server/constants.js
@@ -1,3 +1,15 @@
+export const USER_ACCESS_LEVEL = 1;
+
+export const ADMIN_ACCESS_LEVEL = 2;
+
+export const USER_ROLE_NAME = 'Delegated Admin - User';
+
+export const ADMIN_ROLE_NAME = 'Delegated Admin - Administrator';
+
+export const USER_PERMISSION = 'manage:users';
+
+export const ADMIN_PERMISSION = 'manage:config';
+
 export const VALID_SCRIPTS = [
   'access',
   'filter',
