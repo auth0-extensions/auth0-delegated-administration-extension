4ea9ec627b7a59fd12363822c98770e8790e44b8
Carlos Mostek <carlos.mostek@auth0.com>
2017-08-23 10:52:41 -0500

    add ability to grab css from local CDN as well

--- server/routes/html.js
+++ server/routes/html.js
@@ -6,37 +6,39 @@ import { urlHelpers } from 'auth0-extension-express-tools';
 import config from '../lib/config';
 
 export default () => {
-  const template = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <title><%= config.TITLE %></title>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="shortcut icon" href="https://cdn.auth0.com/styleguide/4.6.13/lib/logos/img/favicon.png">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" type="text/css" href="https://cdn.auth0.com/styles/zocial.min.css" />
-      <link rel="stylesheet" type="text/css" href="https://cdn.auth0.com/manage/v0.3.1672/css/index.min.css" />
-      <link rel="stylesheet" type="text/css" href="https://cdn.auth0.com/styleguide/4.6.13/index.min.css" />
-      <% if (assets.style) { %><link rel="stylesheet" type="text/css" href="/app/<%= assets.style %>" /><% } %>
-      <% if (assets.version) { %><link rel="stylesheet" type="text/css" href="<%= assets.cdnPath %>/auth0-delegated-admin.ui.<%= assets.version %>.css" /><% } %>
-      <% if (assets.customCss) { %><link rel="stylesheet" type="text/css" href="<%= assets.customCss %>" /><% } %>
-    </head>
-    <body>
-      <div id="app"></div>
-      <script src="https://cdn.auth0.com/js/auth0/8.9.3/auth0.min.js"></script>
-      <script type="text/javascript" src="//cdn.auth0.com/manage/v0.3.1672/js/bundle.js"></script>
-      <script type="text/javascript">window.config = <%- JSON.stringify(config) %>;</script>
-      <% if (assets.vendors) { %><script type="text/javascript" src="/app/<%= assets.vendors %>"></script><% } %>
-      <% if (assets.app) { %><script type="text/javascript" src="http://localhost:3001/app/<%= assets.app %>"></script><% } %>
-      <% if (assets.version) { %>
-      <script type="text/javascript" src="<%= assets.cdnPath %>/auth0-delegated-admin.ui.vendors.<%= assets.version %>.js"></script>
-      <script type="text/javascript" src="<%= assets.cdnPath %>/auth0-delegated-admin.ui.<%= assets.version %>.js"></script>
-      <% } %>
-    </body>
-    </html>
-    `;
+  let template = '<!DOCTYPE html>';
+  template += `
+<html lang="en">
+<head>
+  <title><%= config.TITLE %></title>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="shortcut icon" href="https://cdn.auth0.com/styleguide/4.6.13/lib/logos/img/favicon.png">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="https://cdn.auth0.com/styles/zocial.min.css" />
+  <link rel="stylesheet" type="text/css" href="https://cdn.auth0.com/manage/v0.3.1672/css/index.min.css" />
+  <link rel="stylesheet" type="text/css" href="https://cdn.auth0.com/styleguide/4.6.13/index.min.css" />
+  <% if (assets.style) { %><link rel="stylesheet" type="text/css" href="/app/<%= assets.style %>" /><% } %>
+  <% if (assets.version) { %><link rel="stylesheet" type="text/css" href="<%= assets.cdnPath %>/auth0-delegated-admin.ui.<%= assets.version %>.css" /><% } %>
+  <% if (assets.customCss) { %><link rel="stylesheet" type="text/css" href="<%= assets.customCss %>" /><% } %>
+</head>
+<body>
+  <div id="app"></div>
+  <script type="text/javascript" src="//cdn.auth0.com/w2/auth0-7.6.1.min.js"></script>
+  <!-- script src="https://cdn.auth0.com/auth0-extend/1/extend-editor.js"></script>
+  <script src="https://cdn.auth0.com/webtask-editor/editors/1/function-editor.js"></script -->
+  <script type="text/javascript" src="//cdn.auth0.com/manage/v0.3.1672/js/bundle.js"></script>
+  <script type="text/javascript">window.config = <%- JSON.stringify(config) %>;</script>
+  <% if (assets.vendors) { %><script type="text/javascript" src="/app/<%= assets.vendors %>"></script><% } %>
+  <% if (assets.app) { %><script type="text/javascript" src="http://localhost:3001/app/<%= assets.app %>"></script><% } %>
+  <% if (assets.version) { %>
+  <script type="text/javascript" src="<%= assets.cdnPath %>/auth0-delegated-admin.ui.vendors.<%= assets.version %>.js"></script>
+  <script type="text/javascript" src="<%= assets.cdnPath %>/auth0-delegated-admin.ui.<%= assets.version %>.js"></script>
+  <% } %>
+</body>
+</html>
+  `;
 
   return (req, res, next) => {
     if (req.url.indexOf('/api') === 0) {
