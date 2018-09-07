6be2a6f22749192689227c9005dd49f9e5789b92
juliayoone <43078280+juliayoone@users.noreply.github.com>
2018-09-07 20:35:06 +0000

    Update html.js
    added favIcon

--- server/routes/html.js
+++ server/routes/html.js
@@ -15,7 +15,7 @@ export default () => {
       <meta charset="UTF-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="shortcut icon" href="https://cdn.auth0.com/styleguide/4.6.13/lib/logos/img/favicon.png">
+      <link rel="shortcut icon" href="<%= assets.favIcon %>" />
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link rel="stylesheet" type="text/css" href="https://cdn.auth0.com/styles/zocial.min.css" />
       <link rel="stylesheet" type="text/css" href="https://cdn.auth0.com/manage/v0.3.1672/css/index.min.css" />
