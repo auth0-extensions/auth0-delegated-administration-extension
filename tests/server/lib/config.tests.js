a141d38edfe22f9261ae84cef69a5ba8cf816dca
Alexander Zarubin <nonexist.void@gmail.com>
2019-03-29 15:53:56 +0200

    custom domains support (#168)
    * custom domains support
    
    * added appliance check for webauth overrides
    
    * tests

--- tests/server/lib/config.tests.js
+++ tests/server/lib/config.tests.js
@@ -8,34 +8,58 @@ import { defaultConfig } from '../../utils/dummyData';
 describe('config', () => {
   describe('#get', () => {
     it('should return setting from provider if configured', () => {
-      config.setProvider((key) => defaultConfig[key], null);
+      config.setProvider(key => defaultConfig[key], null);
 
       expect(config('AUTH0_DOMAIN')).to.equal(defaultConfig.AUTH0_DOMAIN);
     });
 
-    it('should use domain for issuer domain if none defined', () => {
-      config.setProvider((key) => defaultConfig[key], null);
+    it('should use domain for custom domain if none defined', () => {
+      config.setProvider(key => defaultConfig[key], null);
 
-      expect(config('AUTH0_ISSUER_DOMAIN')).to.equal(defaultConfig.AUTH0_DOMAIN);
+      expect(config('AUTH0_CUSTOM_DOMAIN')).to.equal(defaultConfig.AUTH0_DOMAIN);
     });
 
     it('should use issuer if defined', () => {
       const issuerConfig = _.cloneDeep(defaultConfig);
-      issuerConfig.AUTH0_ISSUER_DOMAIN = 'some_domain';
+      issuerConfig.AUTH0_CUSTOM_DOMAIN = 'some_domain';
 
-      config.setProvider((key) => issuerConfig[key], null);
+      config.setProvider(key => issuerConfig[key], null);
 
       expect(config('AUTH0_DOMAIN')).to.equal(defaultConfig.AUTH0_DOMAIN);
-      expect(config('AUTH0_ISSUER_DOMAIN')).to.equal(issuerConfig.AUTH0_ISSUER_DOMAIN);
+      expect(config('AUTH0_CUSTOM_DOMAIN')).to.equal(issuerConfig.AUTH0_CUSTOM_DOMAIN);
 
       // Put provider back or other tests fail!
-      config.setProvider((key) => defaultConfig[key], null);
+      config.setProvider(key => defaultConfig[key], null);
+    });
+
+    it('should detect appliance', () => {
+      const issuerConfig = _.cloneDeep(defaultConfig);
+      issuerConfig.AUTH0_RTA = 'example.com';
+
+      config.setProvider(key => issuerConfig[key], null);
+
+      expect(config('IS_APPLIANCE')).to.equal(true);
+
+      // Put provider back or other tests fail!
+      config.setProvider(key => defaultConfig[key], null);
+    });
+
+    it('should detect non-appliance', () => {
+      const issuerConfig = _.cloneDeep(defaultConfig);
+      issuerConfig.AUTH0_RTA = 'auth0.auth0.com';
+
+      config.setProvider(key => issuerConfig[key], null);
+
+      expect(config('IS_APPLIANCE')).to.equal(false);
+
+      // Put provider back or other tests fail!
+      config.setProvider(key => defaultConfig[key], null);
     });
 
     it('test getValue and setValue', () => {
-      config.setProvider((key) => defaultConfig[key], null);
+      config.setProvider(key => defaultConfig[key], null);
 
-      expect(config.setValue('SOME_KEY', 'something else')).to.not.throw;
+      expect(config.setValue('SOME_KEY', 'something else')).to.not.throw; // eslint-disable-line no-unused-expressions
       expect(config.getValue('SOME_KEY')).to.equal('something else');
     });
 
