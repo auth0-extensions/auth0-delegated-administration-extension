3cc516d0b32cd90865206ddd34ab440095505add
Alexander Zarubin <nonexist.void@gmail.com>
2019-04-12 11:07:49 +0300

    tests fixed

--- tests/mocha.js
+++ tests/mocha.js
@@ -3,6 +3,28 @@ process.env.NODE_ENV = 'test';
 
 // Register babel so that it will transpile ES6 to ES5
 // before our tests run.
-require('babel-register')();
+
+// Register babel so that it will transpile ES6 to ES5
+// before our tests run.
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('@babel/register')({
+  sourceMaps: true,
+  plugins: [
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-class-properties'
+  ],
+  presets: [
+    '@babel/react',
+    [ '@babel/env', {
+      targets: {
+        node: 'current'
+      }
+    } ]
+  ]
+});
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('@babel/polyfill');
 
 require('./initClientTests');
