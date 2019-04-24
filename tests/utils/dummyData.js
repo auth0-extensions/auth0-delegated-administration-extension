880649f4914e74351dff0ec635b031d98594b909
Alexander Zarubin <nonexist.void@gmail.com>
2019-04-24 14:39:44 +0300

    allow removing unconfirmed enrollments (mfa)

--- tests/utils/dummyData.js
+++ tests/utils/dummyData.js
@@ -15,7 +15,7 @@ export const defaultLogs = [
 export const defaultUsers = [
   { email: 'user1@example.com', user_id: 1, app_metadata: { department: 'deptA' } },
   { email: 'user2@example.com', user_id: 2, app_metadata: { department: 'deptA' } },
-  { email: 'user3@example.com', user_id: 3, app_metadata: { department: 'deptA' } },
+  { email: 'user3@example.com', user_id: 3, app_metadata: { department: 'deptA' }, multifactor_last_modified: 'just now' },
   { email: 'user4@example.com', user_id: 4, app_metadata: { department: 'deptB' } },
   { email: 'user5@example.com', user_id: 5, app_metadata: { department: 'deptB' } }
 ];
